e4193bcb742efbd0cfffecc9269d5f1121f41e74
shravan <venkateshwaranshravan@gmail.com>
2021-05-17 10:56:27 +0530

    Enable DJango admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,36 +4,34 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
-
-# Create your models here.
 class UserProfileManager(BaseUserManager):
     """Manager for User Profiles"""
 
-def create_user(self, email, name, password=None):
-    """Create a new user profile"""
-    if not email:
-        raise ValueError('User must have an email address')
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
 
-    email = self.normalize_email(email)
-    user = self.model(email=email, name=name)
-    user.set_password(password)
-    user.save(using=self._db)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
 
-    return user
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
 
     def create_superuser(self, email, name, password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email,name,password)
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using=self._db)
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            return user
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
-
-
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for Users in the system"""
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
